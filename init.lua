6add0ab7b9ccd3192b0817c9013e3ae7ea11879c
danet <deftqml@gmail.com>
2025-02-14 15:31:10 +0700

    Add VSCode specific key mappings for navigation and editor management

--- init.lua
+++ init.lua
@@ -38,15 +38,33 @@ vim.keymap.set({ "n", "x", "i" }, "<C-d>", function()
   require("vscode-multi-cursor").addSelectionToNextFindMatch()
 end)
 
--- Enhanced navigation mappings
--- Insert mode: Alt + hjkl for movement (less likely to conflict)
-vim.keymap.set('i', '<A-h>', '<Left>', { noremap = true, silent = true })
-vim.keymap.set('i', '<A-j>', '<Down>', { noremap = true, silent = true })
-vim.keymap.set('i', '<A-k>', '<Up>', { noremap = true, silent = true })
-vim.keymap.set('i', '<A-l>', '<Right>', { noremap = true, silent = true })
 
--- Visual mode navigation
-vim.keymap.set('v', '<A-h>', '<Left>', { noremap = true, silent = true })
-vim.keymap.set('v', '<A-j>', '<Down>', { noremap = true, silent = true })
-vim.keymap.set('v', '<A-k>', '<Up>', { noremap = true, silent = true })
-vim.keymap.set('v', '<A-l>', '<Right>', { noremap = true, silent = true })
+-- VSCode specific keymaps
+-- File explorer
+vim.keymap.set('n', '<leader>e', '<Cmd>call VSCodeNotify("workbench.action.toggleSidebarVisibility")<CR>', { silent = true })
+vim.keymap.set('n', '<leader>f', '<Cmd>call VSCodeNotify("workbench.action.quickOpen")<CR>', { silent = true })
+
+-- Editor management
+vim.keymap.set('n', '<leader>w', '<Cmd>call VSCodeNotify("workbench.action.closeActiveEditor")<CR>', { silent = true })
+vim.keymap.set('n', '<C-h>', '<Cmd>call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true })
+vim.keymap.set('n', '<C-l>', '<Cmd>call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true })
+vim.keymap.set('n', '<C-k>', '<Cmd>call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true })
+vim.keymap.set('n', '<C-j>', '<Cmd>call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true })
+
+-- Search and replace
+vim.keymap.set('n', '<leader>h', '<Cmd>call VSCodeNotify("workbench.action.findInFiles")<CR>', { silent = true })
+vim.keymap.set('n', '<leader>r', '<Cmd>call VSCodeNotify("editor.action.startFindReplaceAction")<CR>', { silent = true })
+
+-- Code actions
+vim.keymap.set('n', '<leader>ca', '<Cmd>call VSCodeNotify("editor.action.quickFix")<CR>', { silent = true })
+vim.keymap.set('n', '<leader>cf', '<Cmd>call VSCodeNotify("editor.action.formatDocument")<CR>', { silent = true })
+vim.keymap.set('n', '<leader>cr', '<Cmd>call VSCodeNotify("editor.action.rename")<CR>', { silent = true })
+
+-- Peek actions
+vim.keymap.set('n', 'gd', '<Cmd>call VSCodeNotify("editor.action.peekDefinition")<CR>', { silent = true })
+vim.keymap.set('n', 'gr', '<Cmd>call VSCodeNotify("editor.action.goToReferences")<CR>', { silent = true })
+
+-- Terminal
+vim.keymap.set('n', '<leader>t', '<Cmd>call VSCodeNotify("workbench.action.terminal.toggleTerminal")<CR>', { silent = true })
+vim.keymap.set('n', '<leader>tn', '<Cmd>call VSCodeNotify("workbench.action.terminal.new")<CR>', { silent = true })
+vim.keymap.set('n', '<leader>tk', '<Cmd>call VSCodeNotify("workbench.action.terminal.kill")<CR>', { silent = true })
