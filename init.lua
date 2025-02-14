b01069e9f6c7e27c554228c32937a3f2002f6f37
danet <deftqml@gmail.com>
2025-02-14 15:11:16 +0700

    Refactor key mappings and enhance navigation settings

--- init.lua
+++ init.lua
@@ -17,25 +17,36 @@ require("lazy").setup({
   }
 })
 
+-- Basic settings
 vim.g.mapleader = ' ' 
-
--- Save file
-vim.keymap.set('n', '<leader>s', ':w<CR>', { silent = true })
-
--- Write and quit 
-vim.keymap.set('n', '<leader>q', ':wq<CR>', { silent = true })
-
 vim.opt.clipboard = 'unnamedplus' 
 
 -- Search settings
 vim.opt.ignorecase = true    
 vim.opt.smartcase = true 
 
--- Better line navigation for wrapped lines
+-- File operations
+vim.keymap.set('n', '<leader>s', ':w<CR>', { silent = true })
+vim.keymap.set('n', '<leader>q', ':wq<CR>', { silent = true })
+
+-- Line navigation for wrapped lines
 vim.keymap.set('n', 'j', 'gj', { silent = true })
 vim.keymap.set('n', 'k', 'gk', { silent = true })
 
--- Highlight word under cursor
+-- Multi-cursor functionality
 vim.keymap.set({ "n", "x", "i" }, "<C-d>", function()
   require("vscode-multi-cursor").addSelectionToNextFindMatch()
 end)
+
+-- Enhanced navigation mappings
+-- Insert mode: Alt + hjkl for movement (less likely to conflict)
+vim.keymap.set('i', '<A-h>', '<Left>', { noremap = true, silent = true })
+vim.keymap.set('i', '<A-j>', '<Down>', { noremap = true, silent = true })
+vim.keymap.set('i', '<A-k>', '<Up>', { noremap = true, silent = true })
+vim.keymap.set('i', '<A-l>', '<Right>', { noremap = true, silent = true })
+
+-- Visual mode navigation
+vim.keymap.set('v', '<A-h>', '<Left>', { noremap = true, silent = true })
+vim.keymap.set('v', '<A-j>', '<Down>', { noremap = true, silent = true })
+vim.keymap.set('v', '<A-k>', '<Up>', { noremap = true, silent = true })
+vim.keymap.set('v', '<A-l>', '<Right>', { noremap = true, silent = true })
